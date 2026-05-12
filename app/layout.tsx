6ab801f9f4651bf8cc5902c290f09cf559109ded
gabrielrpm <gabrielrpm92@gmail.com>
2026-05-12 10:19:49 -0300

    feat: Google AdSense instalado

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,15 +28,15 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
+      <body className="min-h-full flex flex-col">
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1988739364132910"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-      </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+        {children}
+      </body>
     </html>
   );
 }
